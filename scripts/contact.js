b95316877fd1914ed63ae57562dfd36e122d464f
gracemcherian <grcherian07@gmail.com>
2022-08-04 11:45:04 -0500

    8/4 two

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,4 +1,6 @@
 
 function myFunction() {
-    alert("You Have Successfully Sent Your Message");
+    if ((document.getElementById("flname").value.length>0) && (document.getElementById("emails").value.length>0) && (document.getElementById("message").value.length>0)) {
+        alert("You Have Successfully Sent Your Message");
+    }
 }
